7c7b072196dd97a6f9553816fb2638a2c246cc50
Roger Crist <totem415@gmail.com>
2020-01-14 17:13:54 -0800

    switched to concise arrow function

--- src/App.js
+++ src/App.js
@@ -23,16 +23,15 @@ class App extends Component {
     
     return (
       <div className="App">
-      {list.map(function(item) {
-          return (
-              <div key={item.objectID}> <span>
+      {list.map(item => 
+            <div key={item.objectID}> <span>
                 <a href={item.url}>{item.title}</a>
               </span>
               <span>{item.author}</span>
               <span>{item.num_comments}</span>
               <span>{item.points}</span>
-            </div> );
-        })}
+            </div> 
+        )}
            
        </div>
      );
