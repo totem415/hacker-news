3ba44dc92df806de5160b86c947841ff90b9bb91
Roger Crist <totem415@gmail.com>
2020-01-14 16:48:22 -0800

    First commit... not the first state in the book but pretty early on.

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
+
+// won't reload the whole page (losing state and log output) but will reflect
+// changes to the source code.
+if (module.hot) {
+    module.hot.accept();
+  }
