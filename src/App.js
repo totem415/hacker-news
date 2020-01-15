bf44dfa4c8a1082d6cae203326904d623814d307
Roger Crist <totem415@gmail.com>
2020-01-15 01:14:04 -0800

    Added a style class to left justify the text and more.

--- src/App.js
+++ src/App.js
@@ -18,27 +18,41 @@ const list = [
     objectID: 1,
 }, ];
 
+const divStyle = {
+  textAlign: "left",
+};
+
 class App extends Component {
   constructor(props) {
       super(props);
       this.state = {
-        list: list,
+        list,
       }
   }
 
   render() {
     return (
-      <div className="App">
+      <div className="App" style={divStyle}>
+        <ul>
       {this.state.list.map(item => 
-            <div key={item.objectID}> <span>
-                <a href={item.url}>{item.title}</a>
+            <div key={item.objectID}> <li><span>
+                <a href={item.url}>{item.title} </a>
               </span>
               <span>{item.author}</span>
               <span>{item.num_comments}</span>
               <span>{item.points}</span>
+              <span>
+                <button
+                  onClick={() => this.onDismiss(item.objectID)}
+                  type="button"
+                > Dismiss
+                </button>
+              </span>
+              </li>
             </div> 
         )}
            
+           </ul>
        </div>
      );
   }
