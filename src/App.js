8aa799e4cd2616c7a86789d2dc087f5b5623be85
Roger Crist <totem415@gmail.com>
2020-01-14 18:05:51 -0800

    Building a component with state.

--- src/App.js
+++ src/App.js
@@ -19,11 +19,17 @@ const list = [
 }, ];
 
 class App extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+        list: list,
+      }
+  }
+
   render() {
-    
     return (
       <div className="App">
-      {list.map(item => 
+      {this.state.list.map(item => 
             <div key={item.objectID}> <span>
                 <a href={item.url}>{item.title}</a>
               </span>
